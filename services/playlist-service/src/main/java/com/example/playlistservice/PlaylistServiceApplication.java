15e00f902cd08e212dd33829b119a7b48d30b6c0
james65 <jamestonibor65@gmail.com>
2026-07-01 11:04:18 +0100

    playlist service config complete and ready to tet with git config

--- services/playlist-service/src/main/java/com/example/playlistservice/PlaylistServiceApplication.java
+++ services/playlist-service/src/main/java/com/example/playlistservice/PlaylistServiceApplication.java
@@ -1,15 +1,31 @@
 package com.example.playlistservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableFeignClients
+@EnableDiscoveryClient
 public class PlaylistServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PlaylistServiceApplication.class, args);
+
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+
+        SpringApplication.run(PlaylistServiceApplication.class, args);
+
     }
 
 }
--- services/song-service/src/main/java/com/example/songservice/SongServiceApplication.java
+++ services/song-service/src/main/java/com/example/songservice/SongServiceApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import io.github.cdimascio.dotenv.Dotenv;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,6 +15,17 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class SongServiceApplication {
 
     public static void main(String[] args) {
+        SpringApplication.run(SongServiceApplication.class, args);
+
+        // Load .env BEFORE Spring starts resolving properties
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+
         SpringApplication.run(SongServiceApplication.class, args);
     }
 
